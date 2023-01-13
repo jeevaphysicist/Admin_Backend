42ab200010453d582540ab51e11b3d3a65c21ed4
jeevaphysicist <jeevaphysicist@gmail.com>
2023-01-13 16:17:25 +0530

    Code Submission

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
-const PORT  = 8080;
+const PORT  = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
