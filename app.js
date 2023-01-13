bc2828398cfba28da26bd7038c5783fd3e26dfbb
jeevaphysicist <jeevaphysicist@gmail.com>
2023-01-13 15:46:53 +0530

    Code Submission

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT  = 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+           res.send(`Default API is Running Perfectly`);
+})
+
+app.listen(PORT,()=>{
+    console.log("Server Running On http://localhost:",PORT);
+})
